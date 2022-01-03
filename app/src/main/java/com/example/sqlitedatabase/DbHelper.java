b1e505525c2d6884038d07960f10f6585a5affb9
ahmadnadeem123 <ahmadnadeempucit@gmail.com>
2022-01-03 16:27:31 +0500

    Data Insert, Display

---
+++ app/src/main/java/com/example/sqlitedatabase/DbHelper.java
@@ -0,0 +1,75 @@
+package com.example.sqlitedatabase;
+
+import  android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.media.session.PlaybackState;
+
+import androidx.annotation.Nullable;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+public class DbHelper extends SQLiteOpenHelper {
+
+    public static final String STUDENT_NAME = "STUDENTName";
+    public static final String STUDENT_ID = "STUDENTID";
+    public static final String STUDENT_TABLE = "StudentTable";
+
+    public DbHelper(@Nullable Context context) {
+        super(context, "studentDB.db", null, 4);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableSTatement = "CREATE TABLE " + STUDENT_TABLE + "(" + STUDENT_ID + " Integer PRIMARY KEY AUTOINCREMENT, " + STUDENT_NAME + " Text) ";
+        db.execSQL(createTableSTatement);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + STUDENT_TABLE);
+        onCreate(db);
+    }
+
+    public void  addStudent(String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        //Hash map, as we did in bundles
+        ContentValues cv = new ContentValues();
+
+        cv.put(STUDENT_NAME, name);
+        db.insert(STUDENT_TABLE, null, cv);
+        db.close();
+
+        //NullCoumnHack
+        //long insert =
+        //if (insert == -1) { return false; }
+        //else{return true;}
+    }
+
+
+    public ArrayList<String> getAllStudents() {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursorCourses = db.rawQuery("SELECT * FROM " + STUDENT_TABLE, null);
+
+        ArrayList<String> studentArrayList = new ArrayList<>();
+
+        // moving our cursor to first position.
+        if (cursorCourses.moveToFirst()) {
+            do {
+
+                studentArrayList.add(new String(cursorCourses.getString(1)));
+
+            } while (cursorCourses.moveToNext());
+
+        }
+
+        cursorCourses.close();
+        return studentArrayList;
+    }
+
+}
--- app/src/main/java/com/example/sqlitedatabase/MainActivity.java
+++ app/src/main/java/com/example/sqlitedatabase/MainActivity.java
@@ -3,12 +3,59 @@ package com.example.sqlitedatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button b1,b2,b3,b4;
+    EditText e1;
+    ListView v1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        b1=findViewById(R.id.b1);
+        b2=findViewById(R.id.b2);
+        e1=findViewById(R.id.e1);
+        v1=findViewById(R.id.listview);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                DbHelper dbHelper = new DbHelper(MainActivity.this);
+                dbHelper.addStudent(e1.getText().toString());
+                e1.setText("");
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DbHelper dbHelper = new DbHelper(MainActivity.this);
+                List<String> list = dbHelper.getAllStudents();
+                ArrayAdapter arrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,list);
+                v1.setAdapter(arrayAdapter);
+
+
+
+            }
+        });
+
+
+
     }
 }
